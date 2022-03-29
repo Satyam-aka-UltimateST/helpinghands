011293fd5b6e39eab8c8cd4c94c866b174dcc908
Satyam Tripathi <98680993+Satyam-aka-UltimateST@users.noreply.github.com>
2022-03-29 19:56:26 +0530

    Add files via upload
    Added google forms

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button")
+button.onclick=()=>{
+    document. getElementById("button"). onclick = function () {
+        location. href = "https://forms.gle/yASK9TmBeBu4LiP38";
+        };
+}
